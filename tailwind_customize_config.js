4ab331cf4edc9fd6b1fdfba03e91976bb5e4af0e
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-08-22 16:48:22 +0600

    initial project setup done

---
+++ tailwind_customize_config.js
@@ -0,0 +1,10 @@
+// customize configuration with own design tokens
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                reddy: '#FF0000',
+            }
+        }
+    }
+}
