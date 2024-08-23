0c9de796776c2f7dd76ada83725621c300220e13
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-08-23 11:40:04 +0600

    Add responsive header using HTML and Tailwind CSS

--- tailwind_customize_config.js
+++ tailwind_customize_config.js
@@ -3,7 +3,14 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-                reddy: '#FF0000',
+                primary_green: 'rgb(171, 239, 95)',
+                light_green: 'rgba(171, 239, 95, 0.5)',
+                shade_greeen: 'rgba(171, 239, 95, 0.2)',
+                primary_black: 'rgb(33, 33, 33)',
+                secondary_black: 'rgb(28, 28, 28)',
+                primary_shade_black: 'rgb(73, 73, 73)',
+                secondary_shade_black: 'rgb(115, 115, 115)',
+                background: '#3e3e3e38',
             }
         }
     }
